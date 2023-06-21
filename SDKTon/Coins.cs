d372ecbc3137f5466b6735a5c5cff9b3feaf9002
purpleguy99 <matthewkalashnikov2002@gmail.com>
2023-06-21 08:43:00 +0300

    - [init] address & coins

---
+++ SDKTon/Coins.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Drawing;
+
+public class CoinsOptions
+{
+    public bool IsNano { get; set; }
+    public int Decimals { get; set; }
+
+    public CoinsOptions(bool IsNano, int Decimals) 
+    {
+        this.IsNano = IsNano;
+        this.Decimals = Decimals;
+    }
+}
+
+public class Coins
+{
+    private decimal Value { get; set; }
+    private int Decimals { get; set; }
+    private decimal Multiplier { get; set; }
+
+    public Coins(object value, CoinsOptions? options = null)
+    {
+        bool isNano = false;
+        int decimals = 9;
+
+        if (options != null)
+        {
+            isNano = options?.IsNano != null ? options.IsNano : false;
+            decimals = options?.Decimals != null ? options.Decimals : 9;
+        }
+
+        CheckCoinsType(value);
+        CheckCoinsDecimals(decimals);
+
+        decimal decimalValue = decimal.Parse(value.ToString());
+
+        int digitsValue = GetDigitsAfterDecimalPoint(decimalValue);
+        if (digitsValue > decimals)
+        {
+            throw new Exception($"Invalid Coins value, decimals places \"{digitsValue}\" can't be greater than selected \"{decimals}\"");
+        }
+
+        Decimals = decimals;
+        Multiplier = (1 * 10) ^ Decimals;
+        Value = !isNano ? decimalValue * Multiplier : decimalValue;
+    }
+
+    public Coins Add(Coins coins)
+    {
+        CheckCoins(coins);
+        CompareCoinsDecimals(this, coins);
+
+        Value = decimal.Add(coins.Value, Value);
+        return this;
+    }
+
+    public Coins Sub(Coins coins)
+    {
+        CheckCoins(coins);
+        CompareCoinsDecimals(this, coins);
+
+        Value = decimal.Subtract(Value, coins.Value);
+        return this;
+    }
+
+    public Coins Mul(object value)
+    {
+        CheckValue(value);
+        CheckConvertability(value);
+
+        var multiplier = Convert.ToInt64(value);
+
+        Value = decimal.Multiply(Value, multiplier);
+        return this;
+    }
+
+    public Coins Div(object value)
+    {
+        CheckValue(value);
+        CheckConvertability(value);
+
+        var divider = Convert.ToInt64(value);
+
+        Value = decimal.Divide(Value, divider);
+        return this;
+    }
+
+    public bool Eq(Coins coins)
+    {
+        CheckCoins(coins);
+        CompareCoinsDecimals(this, coins);
+        return Value == coins.Value;
+    }
+
+    public bool Gt(Coins coins)
+    {
+        CheckCoins(coins);
+        CompareCoinsDecimals(this, coins);
+        return Value > coins.Value;
+    }
+
+    public bool Gte(Coins coins)
+    {
+        CheckCoins(coins);
+        CompareCoinsDecimals(this, coins);
+        return Value >= coins.Value;
+    }
+
+    public bool Lt(Coins coins)
+    {
+        CheckCoins(coins);
+        CompareCoinsDecimals(this, coins);
+        return Value < coins.Value;
+    }
+
+    public bool Lte(Coins coins)
+    {
+        CheckCoins(coins);
+        CompareCoinsDecimals(this, coins);
+        return Value <= coins.Value;
+    }
+
+    public bool IsNegative() => Value < 0;
+
+    public bool IsPositive() => Value > 0;
+
+    public bool IsZero() => Value == 0;
+
+    public string ToNano() => Value.ToString("F0");
+
+    private static void CheckCoinsType(object value)
+    {
+        if (IsValid(value) && IsConvertable(value)) return;
+        if (IsCoins(value)) return;
+
+        throw new Exception("Invalid Coins value");
+    }
+
+    private static void CheckCoinsDecimals(int decimals)
+    {
+        if (decimals < 0 || decimals > 18)
+        {
+            throw new Exception("Invalid decimals value, must be 0-18");
+        }
+    }
+
+    private static void CheckCoins(object value)
+    {
+        if (IsCoins(value)) return;
+        throw new Exception("Invalid value");
+    }
+
+    private static void CompareCoinsDecimals(Coins a, Coins b)
+    {
+        if (a.Decimals != b.Decimals)
+        {
+            throw new Exception("Can't perform mathematical operation of Coins with different decimals");
+        }
+    }
+
+    private static void CheckValue(object value)
+    {
+        if (IsValid(value)) return;
+        throw new Exception("Invalid value");
+    }
+
+    private static void CheckConvertability(object value)
+    {
+        if (IsConvertable(value)) return;
+
+        throw new Exception("Invalid value");
+    }
+
+    private static bool IsValid(object value)
+    {
+        // TODO: узнать про тип данных
+        return value is string || value is int || value is decimal;
+    }
+
+    private static bool IsConvertable(object value)
+    {
+        try
+        {
+            new decimal(Convert.ToDouble(value.ToString()));
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    private static bool IsCoins(object value)
+    {
+        return value is Coins;
+    }
+
+    private static int GetDigitsAfterDecimalPoint(decimal number)
+    {
+        string[] parts = number.ToString().Split('.');
+        if (parts.Length == 2)
+        {
+            return parts[1].Length;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public static Coins FromNano(object value, int decimals = 9)
+    {
+        CheckCoinsType(value);
+        CheckCoinsDecimals(decimals);
+
+        return new Coins(value, new CoinsOptions(false, decimals));
+    }
+
+    // TODO: toString() method 
+}
---
+++ SDKTon/Utils.cs
@@ -0,0 +1,89 @@
+﻿public static class Utils
+{
+    public static ushort Crc16(byte[] data)
+    {
+        const ushort POLY = 0x1021;
+        ushort crc = 0;
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            crc ^= (ushort)(data[i] << 8);
+
+            for (int j = 0; j < 8; j++)
+            {
+                if ((crc & 0x8000) == 0x8000)
+                {
+                    crc = (ushort)((crc << 1) ^ POLY);
+                }
+                else
+                {
+                    crc <<= 1;
+                }
+            }
+        }
+
+        return (ushort)(crc & 0xffff);
+    }
+
+    public static byte[] Crc16BytesBigEndian(byte[] data)
+    {
+        ushort crc = Crc16(data);
+        byte[] bytes = new byte[2];
+
+        bytes[0] = (byte)(crc >> 8);
+        bytes[1] = (byte)crc;
+
+        return bytes;
+    }
+
+    public static uint Crc32c(byte[] data)
+    {
+        const uint POLY = 0x82f63b78;
+        uint crc = 0xffffffff;
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            crc ^= data[i];
+
+            for (int j = 0; j < 8; j++)
+            {
+                if ((crc & 1) == 1)
+                {
+                    crc = (crc >> 1) ^ POLY;
+                }
+                else
+                {
+                    crc >>= 1;
+                }
+            }
+        }
+
+        crc ^= 0xffffffff;
+
+        return crc;
+    }
+
+    public static byte[] Crc32cBytesLittleEndian(byte[] data)
+    {
+        uint crc = Crc32c(data);
+        byte[] bytes = new byte[4];
+
+        bytes[0] = (byte)crc;
+        bytes[1] = (byte)(crc >> 8);
+        bytes[2] = (byte)(crc >> 16);
+        bytes[3] = (byte)(crc >> 24);
+
+        return bytes;
+    }
+
+    public static short ToLittleEndianInt16(this byte[] bytes, int startIndex = 0)
+    {
+        if (bytes.Length - startIndex < 2)
+        {
+            throw new ArgumentException("Byte array too small to convert to Int16");
+        }
+
+        return (short)(bytes[startIndex] | (bytes[startIndex + 1] << 8));
+    }
+
+}
