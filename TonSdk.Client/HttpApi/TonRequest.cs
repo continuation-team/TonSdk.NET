abdb10603323939449fa804d7e221e15bdb92bc4
purpleguy99 <matthewkalashnikov2002@gmail.com>
2023-06-25 17:35:48 +0300

    - [update] add getTransactions and account data

--- TonSdk.Client/HttpApi/TonRequest.cs
+++ TonSdk.Client/HttpApi/TonRequest.cs
@@ -22,11 +22,11 @@ public class TonRequest
     private readonly RequestParameters _params;
     public TonRequest(RequestParameters _params) => this._params = _params;
 
-    public async Task<object> Call()
+    public async Task<string> Call()
     {
         HttpClient httpClient = new();
         httpClient.Timeout = TimeSpan.FromMilliseconds(Convert.ToDouble(_params.HttpApiParameters.Timeout));
-        httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
+        //httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
         
         if(_params.HttpApiParameters.ApiKey != null && _params.HttpApiParameters.ApiKey != string.Empty)
             httpClient.DefaultRequestHeaders.Add("X-API-Key", _params.HttpApiParameters.ApiKey);
@@ -36,14 +36,15 @@ public class TonRequest
             id = "1",
             jsonrpc = "2.0",
             method = _params.MethodName,
-            @params = _params.RequestBody != null ? JsonConvert.SerializeObject(_params.RequestBody) : null
+            @params = _params.RequestBody != null ? _params.RequestBody : null
         });
 
         StringContent content = new(data, System.Text.Encoding.UTF8, "application/json");
         HttpResponseMessage response = await httpClient.PostAsync(_params.HttpApiParameters.Endpoint, content);
 
         if (!response.IsSuccessStatusCode) throw new Exception($"Received error: {await response.Content.ReadAsStringAsync()}");
-        var result = JsonConvert.DeserializeObject<dynamic>(await response.Content.ReadAsStringAsync());
+        string result = await response.Content.ReadAsStringAsync();
+
         return result;
     }
 }
