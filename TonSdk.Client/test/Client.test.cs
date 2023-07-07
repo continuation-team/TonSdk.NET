a2fb301d6772ffe3e3dcd976c5266532d468c9f7
purpleguy99 <matthewkalashnikov2002@gmail.com>
2023-07-07 05:01:15 +0300

    Update Client.test.cs

--- TonSdk.Client/test/Client.test.cs
+++ TonSdk.Client/test/Client.test.cs
@@ -18,4 +18,20 @@ public class ClientTest
         Assert.That(await client.IsContractDeployed(new Address("EQCD39VS5jcptHL8vMjEXrzGaRcCVYto7HUn4bpAOg8xqB2N")), Is.EqualTo(true));
         Assert.That(await client.IsContractDeployed(new Address("EQCD39VS5jcptHL8vMjEXrzGaRcCVYto7HAn4bpAOg8xofto")), Is.EqualTo(false));
     }
+
+    [Test]
+    public async Task Test_WalletGetSeqno()
+    {
+        Assert.That(await client.Wallet.GetSeqno(new Address("EQCD39VS5jcptHL8vMjEXrzGaRcCVYto7HUn4bpAOg8xqB2N")) > 0, Is.EqualTo(true));
+        Assert.That(await client.Wallet.GetSeqno(new Address("EQCD39VS5jcptHL8vMjEXrzGaRcCVYto7HAn4bpAOg8xofto")), Is.EqualTo(null));
+    }
+
+    [Test]
+    public async Task Test_NftGetItemAddress()
+    {
+        Assert.That((await client.Nft.GetItemAddress(new Address("EQBiX_Sxuy5htTLg_BzUk1kw7FuI3zWvmnotiuvmnwqZpmUj"), 0)).Equals(new Address("EQCD4WUntH5862PfOQc1vsGAPZsjnm49Mxrj-23O01B_YaJ2")), Is.EqualTo(true));
+        Assert.ThrowsAsync<Exception>(async Task() => await client.Nft.GetItemAddress(new Address("EQCD39VS5jcptHL8vMjEXrzGaRcCVYto7HAn4bpAOg8xofto"), 2), "Cannot retrieve nft address.");
+    }
+
+
 }
