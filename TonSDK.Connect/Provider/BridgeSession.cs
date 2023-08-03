df7409606e8a5884eaaafa7a6234284f5fbc11db
purpleguy99 <matthewkalashnikov2002@gmail.com>
2023-08-03 18:53:35 +0300

    - [update] add session classes (Bridge & Info)

---
+++ TonSDK.Connect/Provider/BridgeSession.cs
@@ -0,0 +1,15 @@
+﻿namespace TonSdk.Connect;
+
+public struct BridgeSession
+{
+    public CryptedSessionInfo? CryptedSessionInfo { get; private set; }
+    public string? WalletPublicKey { get; private set; }
+    public string? BridgeUrl { get; private set; }
+
+    public BridgeSession(SessionInfo sessionInfo)
+    {
+        CryptedSessionInfo = sessionInfo.SessionPrivateKey != null ? new(sessionInfo.SessionPrivateKey) : null;
+        WalletPublicKey = sessionInfo.WalletPublicKey ?? null;
+        BridgeUrl = sessionInfo.BridgeUrl ?? null;
+    }
+}
---
+++ TonSDK.Connect/Provider/SessionInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using TonSdk.Core.Crypto;
+
+namespace TonSdk.Connect;
+
+public struct CryptedSessionInfo
+{
+    public KeyPair KeyPair { get; private set; }
+    public string SesionId { get; private set; }
+
+
+    public CryptedSessionInfo(string? seed = null)
+    {
+        byte[] seedBytes = seed == null ? GenerateRandomBytes(32) : Utils.HexToBytes(seed);
+        KeyPair = Utils.GenerateKeyPair(seedBytes);
+        SesionId = Utils.BytesToHex(KeyPair.PublicKey);
+    }
+
+    private static byte[] GenerateRandomBytes(int byteSize)
+    {
+        using RandomNumberGenerator randomNumberGenerator = RandomNumberGenerator.Create();
+        byte[] randomBytes = new byte[byteSize];
+        randomNumberGenerator.GetBytes(randomBytes);
+        return randomBytes;
+    }
+}
+
+public struct SessionInfo
+{
+    public string? SessionPrivateKey { get; set; }
+    public string? WalletPublicKey { get; set; }
+    public string? BridgeUrl { get; set; }
+}
