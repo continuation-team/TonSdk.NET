92094348d2e586bf09f174b8f01835e22f55796b
aslan bozieff <neuralink7232050@gmail.com>
2023-10-25 22:20:50 +0300

    added HighloadV1

--- TonSdk.Contracts/src/wallet/HighloadV1.cs
+++ TonSdk.Contracts/src/wallet/HighloadV1.cs
@@ -3,58 +3,64 @@ using TonSdk.Core;
 using TonSdk.Core.Block;
 using TonSdk.Core.Boc;
 
-namespace TonSdk.Contracts.Wallet {
-    public struct WalletV3Options {
+namespace TonSdk.Contracts.Wallet
+{
+    public struct HighloadV1Options
+    {
         public byte[] PublicKey;
         public int? Workchain;
         public uint? SubwalletId;
     }
 
-    public struct WalletV3Storage {
+    public struct HighloadV1Storage
+    {
         public uint Seqno;
-        public uint SubwalletId;
+        public uint? SubwalletId;
         public byte[] PublicKey;
     }
 
 
-    public class WalletV3 : WalletBase {
+    public class HighloadV1 : WalletBase
+    {
         private uint _subwalletId;
 
         public uint SubwalletId => _subwalletId;
 
-        public WalletV3(WalletV3Options opt, uint revision = 2) {
-            if (revision != 1 && revision != 2) {
-                throw new Exception("Unsupported revision. Only 1 and 2 are supported");
-            }
-
-            _code = revision == 1 ? Cell.From(WalletSources.V3R1) : Cell.From(WalletSources.V3R2);
-            _publicKey = opt.PublicKey;
+        public HighloadV1(HighloadV1Options opt)
+        {
+            _code = Cell.From(WalletSources.HighloadV1);
             _subwalletId = opt.SubwalletId ?? WalletTraits.SUBWALLET_ID;
+            _publicKey = opt.PublicKey;
             _stateInit = buildStateInit();
             _address = new Address(opt.Workchain ?? 0, _stateInit);
         }
 
-        public WalletV3Storage ParseStorage(CellSlice slice) {
+        public HighloadV1Storage ParseStorage(CellSlice slice)
+        {
             BlockUtils.CheckUnderflow(slice, 32 + 32 + 256, null);
-            return new WalletV3Storage {
+            return new HighloadV1Storage
+            {
                 Seqno = (uint)slice.LoadUInt(32),
                 SubwalletId = (uint)slice.LoadUInt(32),
                 PublicKey = slice.LoadBytes(32)
             };
         }
 
-        protected sealed override StateInit buildStateInit() {
+        protected sealed override StateInit buildStateInit()
+        {
             var data = new CellBuilder()
-                .StoreUInt(0, 32)
+                .StoreUInt(0, 32) // seqno
                 .StoreUInt(_subwalletId, 32)
                 .StoreBytes(_publicKey)
                 .Build();
             return new StateInit(new StateInitOptions { Code = _code, Data = data });
         }
 
-        public ExternalInMessage CreateTransferMessage(WalletTransfer[] transfers, uint seqno, uint timeout = 60) {
-            if (transfers.Length == 0 || transfers.Length > 4) {
-                throw new Exception("WalletV3: can make only 1 to 4 transfers per operation.");
+        public ExternalInMessage CreateTransferMessage(WalletTransfer[] transfers, uint seqno, uint timeout = 60)
+        {
+            if (transfers.Length == 0 || transfers.Length > 255)
+            {
+                throw new Exception("HighloadV1: can make only 1 to 255 transfers per operation.");
             }
 
             var bodyBuilder = new CellBuilder()
@@ -62,26 +68,40 @@ namespace TonSdk.Contracts.Wallet {
                 .StoreUInt(DateTimeOffset.Now.ToUnixTimeSeconds() + timeout, 32)
                 .StoreUInt(seqno, 32);
 
-            foreach (var transfer in transfers) {
-                bodyBuilder
-                    .StoreUInt(transfer.Mode, 8)
-                    .StoreRef(transfer.Message.Cell);
+            var actions = new OutAction[transfers.Length];
+
+            for (var i = 0; i < transfers.Length; i++)
+            {
+                var transfer = transfers[i];
+                var action = new ActionSendMsg(new ActionSendMsgOptions
+                {
+                    Mode = transfer.Mode,
+                    OutMsg = transfer.Message
+                });
+                actions[i] = action;
             }
 
-            return new ExternalInMessage(new ExternalInMessageOptions {
+            bodyBuilder.StoreRef(new OutList(new OutListOptions { Actions = actions }).Cell);
+
+            return new ExternalInMessage(new ExternalInMessageOptions
+            {
                 Info = new ExtInMsgInfo(new ExtInMsgInfoOptions { Dest = Address }),
                 Body = bodyBuilder.Build(),
                 StateInit = seqno == 0 ? _stateInit : null
             });
         }
 
-        public ExternalInMessage CreateDeployMessage() {
+
+        public ExternalInMessage CreateDeployMessage()
+        {
             var bodyBuilder = new CellBuilder()
                 .StoreUInt(_subwalletId, 32)
                 .StoreInt(-1L, 32)
-                .StoreUInt(0, 32); // seqno = 0
+                .StoreUInt(0, 32) // seqno = 0
+                .StoreBit(false); // empty dict
 
-            return new ExternalInMessage(new ExternalInMessageOptions {
+            return new ExternalInMessage(new ExternalInMessageOptions
+            {
                 Info = new ExtInMsgInfo(new ExtInMsgInfoOptions { Dest = Address }),
                 Body = bodyBuilder.Build(),
                 StateInit = _stateInit
