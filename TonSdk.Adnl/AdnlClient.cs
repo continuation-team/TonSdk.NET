4ea7c3eca9307cf817944d3a37cb968da6120d49
purpleguy99 <matthewkalashnikov2002@gmail.com>
2023-10-31 16:38:21 +0300

    add AdnlKeys.cs

--- TonSdk.Adnl/AdnlClient.cs
+++ TonSdk.Adnl/AdnlClient.cs
@@ -2,12 +2,28 @@
 
 public interface INetworkClient { }
 
+public enum AdnlClientState
+{
+    Connecting,
+    Open,
+    Closing,
+    Closed
+}
+
 public class AdnlClient
 {
-    private INetworkClient _socket;
-    private string _host;
-    private int _port;
+    protected INetworkClient _socket;
+    protected string _host;
+    protected int _port;
+    
     private AdnlAddress _address;
+    private AdnlClientState _state = AdnlClientState.Closed;
+    
+    public event Action Connected;
+    public event Action Ready;
+    public event Action Closed;
+    public event Action<byte[]> DataReceived;
+    public event Action<Exception, bool> ErrorOccurred;
     
     public AdnlClient(INetworkClient socket, string url, byte[] peerPublicKey)
     {
@@ -16,5 +32,10 @@ public class AdnlClient
         _port = uri.Port;
         _address = new AdnlAddress(peerPublicKey);
         _socket = socket;
+    }
+
+    protected async Task OnBeforeConnect()
+    {
+        if (_state != AdnlClientState.Closed) return;
     }
 }
---
+++ TonSdk.Adnl/AdnlKeys.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Agreement;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+
+namespace TonSdk.Adnl;
+
+public class AdnlKeys
+{
+    private byte[] _peer;
+    private byte[] _public;
+    private byte[] _shared;
+
+    public AdnlKeys(byte[] peerPublicKey)
+    {
+        _peer = peerPublicKey;
+    }
+
+    public byte[] Public() => _public;
+    public byte[] Shared() => _shared;
+
+    public void Generate()
+    {
+        byte[] privateKey = GenerateRandomBytes(32);
+        X25519PrivateKeyParameters privateKeyParams = new X25519PrivateKeyParameters(privateKey, 0); 
+        X25519PublicKeyParameters publicKey = privateKeyParams.GeneratePublicKey();
+        byte[] sharedSecret = new byte[32];
+        privateKeyParams.GenerateSecret(publicKey, sharedSecret, 0);
+        _shared = sharedSecret;
+        _public = publicKey.GetEncoded();
+    }
+    
+    private static byte[] GenerateRandomBytes(int byteSize)
+    {
+        using RandomNumberGenerator randomNumberGenerator = RandomNumberGenerator.Create();
+        byte[] randomBytes = new byte[byteSize];
+        randomNumberGenerator.GetBytes(randomBytes);
+        return randomBytes;
+    }
+}
