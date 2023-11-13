8bdd78f3b00db4751edfe79465a0a53d0282439c
purpleguy99 <matthewkalashnikov2002@gmail.com>
2023-11-13 12:44:59 +0300

    down grade library to target frameworks

--- TonSdk.Adnl/Adnl/AdnlCipher.cs
+++ TonSdk.Adnl/Adnl/AdnlCipher.cs
@@ -1,50 +1,47 @@
-﻿using Org.BouncyCastle.Crypto;
-using Org.BouncyCastle.Crypto.Engines;
-using Org.BouncyCastle.Crypto.Modes;
-using Org.BouncyCastle.Crypto.Parameters;
-using System;
-using TonSdk.Adnl;
+﻿using System;
 
-namespace TonSdk.Adnl;
-
-internal class Cipher
+namespace TonSdk.Adnl
 {
-    private readonly AesCtrMode _cipher;
-    
-    internal Cipher(byte[] key, byte[] iv)
+    internal class Cipher
     {
-        if (key.Length != 32)
-            throw new ArgumentException("Invalid key length. Key must be 256 bits.");
+        private readonly AesCtrMode _cipher;
 
-        if (iv.Length != 16)
-            throw new ArgumentException("Invalid IV length. IV must be 128 bits.");
+        internal Cipher(byte[] key, byte[] iv)
+        {
+            if (key.Length != 32)
+                throw new ArgumentException("Invalid key length. Key must be 256 bits.");
 
-        _cipher = new AesCtrMode(key, new AesCounter(iv));
+            if (iv.Length != 16)
+                throw new ArgumentException("Invalid IV length. IV must be 128 bits.");
+
+            _cipher = new AesCtrMode(key, new AesCounter(iv));
+        }
+
+        internal byte[] Update(byte[] data) => _cipher.Encrypt(data);
     }
 
-    internal byte[] Update(byte[] data) => _cipher.Encrypt(data);
-}
-
-internal class Decipher
-{ 
-    private readonly AesCtrMode _cipher;
-    internal Decipher(byte[] key, byte[] iv)
+    internal class Decipher
     {
-        if (key.Length != 32)
-            throw new ArgumentException("Invalid key length. Key must be 256 bits.");
+        private readonly AesCtrMode _cipher;
 
-        if (iv.Length != 16)
-            throw new ArgumentException("Invalid IV length. IV must be 128 bits.");
+        internal Decipher(byte[] key, byte[] iv)
+        {
+            if (key.Length != 32)
+                throw new ArgumentException("Invalid key length. Key must be 256 bits.");
 
-        _cipher = new AesCtrMode(key, new AesCounter(iv));
+            if (iv.Length != 16)
+                throw new ArgumentException("Invalid IV length. IV must be 128 bits.");
+
+            _cipher = new AesCtrMode(key, new AesCounter(iv));
+        }
+
+        internal byte[] Update(byte[] data) => _cipher.Decrypt(data);
     }
 
-    internal byte[] Update(byte[] data) => _cipher.Decrypt(data);
-}
+    internal static class CipherFactory
+    {
+        internal static Cipher CreateCipheriv(byte[] key, byte[] iv) => new Cipher(key, iv);
 
-internal static class CipherFactory
-{
-    internal static Cipher CreateCipheriv(byte[] key, byte[] iv) => new Cipher(key, iv);
-
-    internal static Decipher CreateDecipheriv(byte[] key, byte[] iv) => new Decipher(key, iv);
+        internal static Decipher CreateDecipheriv(byte[] key, byte[] iv) => new Decipher(key, iv);
+    }
 }
--- TonSdk.Adnl/Adnl/AdnlPacket.cs
+++ TonSdk.Adnl/Adnl/AdnlPacket.cs
@@ -1,67 +1,80 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
 using TonSdk.Core.Boc;
 
-namespace TonSdk.Adnl;
-
-internal class AdnlPacket
+namespace TonSdk.Adnl
 {
-    internal const byte PacketMinSize = 68; // 4 (size) + 32 (nonce) + 32 (hash)
-
-    private byte[] _payload;
-    private byte[] _nonce;
-
-    internal AdnlPacket(byte[] payload, byte[]? nonce = null)
+    internal class AdnlPacket
     {
-        _nonce = nonce ?? AdnlKeys.GenerateRandomBytes(32);
-        _payload = payload;
-    }
+        internal const byte PacketMinSize = 68; // 4 (size) + 32 (nonce) + 32 (hash)
 
-    internal byte[] Payload => _payload;
-    
-    private byte[] Nonce => _nonce;
-    
-    private byte[] Hash => SHA256.HashData(_nonce.Concat(_payload).ToArray());
-    
-    private byte[] Size {
-        get
+        private byte[] _payload;
+        private byte[] _nonce;
+
+        internal AdnlPacket(byte[] payload, byte[]? nonce = null)
         {
-            uint size = (uint)(32 + 32 + _payload.Length);
-            Bits builder = new BitsBuilder().StoreUInt32LE(size).Build();
-            return builder.ToBytes();
+            _nonce = nonce ?? AdnlKeys.GenerateRandomBytes(32);
+            _payload = payload;
         }
-    }
-    
-    internal byte[] Data => Size.Concat(Nonce).Concat(Payload).Concat(Hash).ToArray();
 
-    internal int Length => PacketMinSize + _payload.Length;
+        internal byte[] Payload => _payload;
 
-    internal static AdnlPacket? Parse(byte[] data)
-    {
-        if (data.Length < 4) return null;
-        int cursor = 0;
+        private byte[] Nonce => _nonce;
 
-        BitsSlice slice = new Bits(data).Parse();
+        private byte[] Hash
+        {
+            get 
+            { 
+                using SHA256 sha256 = SHA256.Create();
+                byte[] sha256Hash = sha256.ComputeHash(_nonce.Concat(_payload).ToArray());
+                return sha256Hash; 
+            }
+        }
 
-        uint size = (uint)slice.LoadUInt32LE();
-        cursor += 4;
-        
-        if (data.Length - 4 < size) return null;
-        
-        byte[] nonce = new byte[32];
-        Array.Copy(data, cursor, nonce, 0, 32);
-        cursor += 32;
+        private byte[] Size
+        {
+            get
+            {
+                uint size = (uint)(32 + 32 + _payload.Length);
+                Bits builder = new BitsBuilder().StoreUInt32LE(size).Build();
+                return builder.ToBytes();
+            }
+        }
 
-        byte[] payload = new byte[size - (32 + 32)];
-        Array.Copy(data, cursor, payload, 0, size - (32 + 32));
-        cursor += (int)size - (32 + 32);
+        internal byte[] Data => Size.Concat(Nonce).Concat(Payload).Concat(Hash).ToArray();
 
-        byte[] hash = new byte[32];
-        Array.Copy(data, cursor, hash, 0, 32);
-        
-        byte[] target = SHA256.HashData(nonce.Concat(payload).ToArray());
+        internal int Length => PacketMinSize + _payload.Length;
 
-        if (!hash.SequenceEqual(target)) throw new Exception("ADNLPacket: Bad packet hash.");
+        internal static AdnlPacket? Parse(byte[] data)
+        {
+            if (data.Length < 4) return null;
+            int cursor = 0;
 
-        return new AdnlPacket(payload, nonce);
+            BitsSlice slice = new Bits(data).Parse();
+
+            uint size = (uint)slice.LoadUInt32LE();
+            cursor += 4;
+
+            if (data.Length - 4 < size) return null;
+
+            byte[] nonce = new byte[32];
+            Array.Copy(data, cursor, nonce, 0, 32);
+            cursor += 32;
+
+            byte[] payload = new byte[size - (32 + 32)];
+            Array.Copy(data, cursor, payload, 0, size - (32 + 32));
+            cursor += (int)size - (32 + 32);
+
+            byte[] hash = new byte[32];
+            Array.Copy(data, cursor, hash, 0, 32);
+            
+            using SHA256 sha256 = SHA256.Create();
+            byte[] target = sha256.ComputeHash(nonce.Concat(payload).ToArray());
+
+            if (!hash.SequenceEqual(target)) throw new Exception("ADNLPacket: Bad packet hash.");
+
+            return new AdnlPacket(payload, nonce);
+        }
     }
 }
