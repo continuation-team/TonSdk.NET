434ceb0f07800aedad00da02745adc3d42c7f7bb
Purple Guy <89442150+purpleguy99@users.noreply.github.com>
2024-04-17 19:21:03 +0300

    add JettonMinter and NftCollection classes
    * GITBOOK-35: No subject
    
    * GITBOOK-36: No subject
    
    * Add StackItem parsing | Client 0.3.2
    
    * implement JettonMinter.cs
    
    * Add NftCollection.cs
    
    * GITBOOK-37: No subject
    
    * update TonSdk.Contracts.csproj

--- TonSdk.Contracts/src/Utils.cs
+++ TonSdk.Contracts/src/Utils.cs
@@ -1,12 +1,124 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
+using Org.BouncyCastle.Math;
+using TonSdk.Contracts.Jetton;
+using TonSdk.Core.Boc;
+using TonSdk.Core.Crypto;
 
 namespace TonSdk.Contracts {
-    public static class SmcUtils {
+    public static class SmcUtils 
+    {
+        public const uint ONCHAIN_CONTENT_PREFIX = 0x00;
+        public const uint OFFCHAIN_CONTENT_PREFIX = 0x01;
+        public const uint SNAKE_PREFIX = 0x00;
+        public const int CELL_MAX_SIZE_BYTES = 126;
+        
         public static ulong GenerateQueryId(int timeout, int? randomId = null) {
             var now = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
             var random = randomId ?? new Random().Next(0, (int)Math.Pow(2, 30));
 
             return (ulong)((now + timeout) << 32) | (uint)random;
+        }
+
+        public static Cell CreateOffChainUriCell(string uri)
+        {
+            return new CellBuilder()
+                .StoreUInt(OFFCHAIN_CONTENT_PREFIX, 8)
+                .StoreBytes(Encoding.UTF8.GetBytes(uri))
+                .Build();
+        }
+        
+        private static List<byte[]> BufferToChunks(byte[] buff, int chunkSize)
+        {
+            List<byte[]> chunks = new List<byte[]>();
+            int currentIndex = 0;
+
+            while (buff.Length - currentIndex > 0)
+            {
+                int currentChunkSize = Math.Min(chunkSize, buff.Length - currentIndex);
+                byte[] chunk = new byte[currentChunkSize];
+                Array.Copy(buff, currentIndex, chunk, 0, currentChunkSize);
+                chunks.Add(chunk);
+                currentIndex += currentChunkSize;
+            }
+
+            return chunks;
+        }
+
+        private static Cell MakeSnakeCell(byte[] data)
+        {
+            var chunks = BufferToChunks(data, CELL_MAX_SIZE_BYTES);
+            var currentCell = new CellBuilder();
+
+            for (int i = chunks.Count - 1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    currentCell.StoreInt(SNAKE_PREFIX, 8);
+                }
+
+                currentCell.StoreBytes(chunks[i]);
+
+                if (i > 0)
+                {
+                    Cell finishedCell = currentCell.Build();
+                    currentCell = new CellBuilder();
+                    currentCell.StoreRef(finishedCell);
+                }
+            }
+
+            return new CellBuilder().StoreRef(currentCell.Build()).Build();
+        }
+        
+        public static Cell CreateOnChainUriCell(JettonOnChainContent contentStorage)
+        {
+            var hmOptions = new HashmapOptions<byte[], Cell>()
+            {
+                KeySize = 256,
+                Serializers = new HashmapSerializers<byte[], Cell>
+                {
+                    Key = k => new BitsBuilder(256).StoreBytes(k).Build(),
+                    Value = v => v
+                },
+                Deserializers = null
+            };
+            
+            var hm = new HashmapE<byte[], Cell>(hmOptions);
+
+            if (contentStorage.Name == null || contentStorage.Symbol == null || contentStorage.Description == null)
+                throw new Exception("Jetton must contain required fields - name, symbol and description.");
+            
+            hm.Set(Utils.HexToBytes("82a3537ff0dbce7eec35d69edc3a189ee6f17d82f353a553f9aa96cb0be3ce89"),
+                MakeSnakeCell(Encoding.UTF8.GetBytes(contentStorage.Name!)));
+            hm.Set(Utils.HexToBytes("c9046f7a37ad0ea7cee73355984fa5428982f8b37c8f7bcec91f7ac71a7cd104"),
+                MakeSnakeCell(Encoding.UTF8.GetBytes(contentStorage.Description!)));
+            hm.Set(Utils.HexToBytes("b76a7ca153c24671658335bbd08946350ffc621fa1c516e7123095d4ffd5c581"),
+                MakeSnakeCell(Encoding.UTF8.GetBytes(contentStorage.Symbol!)));
+            
+            if(contentStorage.Decimals != null)
+                hm.Set(Utils.HexToBytes("ee80fd2f1e03480e2282363596ee752d7bb27f50776b95086a0279189675923e"),
+                    MakeSnakeCell(Encoding.UTF8.GetBytes(contentStorage.Decimals.ToString())));
+            if(contentStorage.Image != null)
+                hm.Set(Utils.HexToBytes("6105d6cc76af400325e94d588ce511be5bfdbb73b437dc51eca43917d7a43e3d"),
+                    MakeSnakeCell(Encoding.UTF8.GetBytes(contentStorage.Image)));
+            if(contentStorage.Uri != null)
+                hm.Set(Utils.HexToBytes("70e5d7b6a29b392f85076fe15ca2f2053c56c2338728c4e33c9e8ddb1ee827cc"),
+                    MakeSnakeCell(Encoding.UTF8.GetBytes(contentStorage.Uri)));
+            if(contentStorage.ImageData != null)
+                hm.Set(Utils.HexToBytes("d9a88ccec79eef59c84b671136a20ece4cd00caaad5bc47e2c208829154ee9e4"),
+                    MakeSnakeCell(Encoding.UTF8.GetBytes(contentStorage.ImageData)));
+            if(contentStorage.RenderType != null)
+                hm.Set(Utils.HexToBytes("d33ae06043036d0d1c3be27201ac15ee4c73da8cdb7c8f3462ce308026095ac0"),
+                    MakeSnakeCell(Encoding.UTF8.GetBytes(contentStorage.RenderType)));
+            if(contentStorage.AmountStyle != null)
+                hm.Set(Utils.HexToBytes("8b10e058ce46c44bc1ba139bc9761721e49170e2c0a176129250a70af053b700"),
+                    MakeSnakeCell(Encoding.UTF8.GetBytes(contentStorage.AmountStyle)));
+            
+            return new CellBuilder()
+                .StoreUInt(ONCHAIN_CONTENT_PREFIX, 8)
+                .StoreDict(hm)
+                .Build();
         }
     }
 }
---
+++ docs/user-manual/use-cases/create-and-mint-jettons.md
@@ -0,0 +1,154 @@
+# Create and Mint Jettons
+
+In this topic you will know how to create new Jetton and mint it using `TonSDK.Net`
+
+#### Preparing Metadata
+
+You can create Jettons with `OffChain` or `OnChain` metadata using structs in `TonSdk.Contracts.Jetton`:
+
+<pre class="language-csharp"><code class="lang-csharp"><strong>// offchain metadata 
+</strong>JettonMinterOptions options = new JettonMinterOptions()
+{
+    AdminAddress = new Address(/* admin address */),
+    JettonContent = new JettonOffChainContent()
+    {
+        ContentUri = "https://example.com/metadata.json" // your metadata link
+    }
+};
+</code></pre>
+
+```csharp
+// onchain metadata 
+JettonMinterOptions options = new JettonMinterOptions()
+{
+    AdminAddress = new Address(/* admin address */),
+    JettonContent = new JettonOnChainContent()
+    {
+        Name = "My Best Token",
+        Symbol = "MBT",
+        Description = "This is the best description."
+        Decimals = 9, // optional
+        Image = "https://example.com/image.png" // optional
+    }
+};
+```
+
+#### Creating new Jetton Minter
+
+To create new jetton, you can use `JettonMinter` class to create it with `JettonMinterOptions` presented above.
+
+{% hint style="info" %}
+For example, we will use `WalletV4`. You can find more examples for different contracts [here](../contracts/wallet/).
+{% endhint %}
+
+```csharp
+// create http parameters for ton client 
+HttpParameters tonClientParams = new HttpParameters 
+{
+    Endpoint = "https://toncenter.com/api/v2/jsonRPC",
+    ApiKey = "xxx" 
+};
+
+// create ton client to fetch data and send boc
+TonClient tonClient = new TonClient(TonClientType.HTTP_TONCENTERAPIV2, tonClientParams);
+
+// create new mnemonic or use existing
+Mnemonic mnemonic = new Mnemonic();
+
+// create wallet options
+WalletV4Options options = new WalletV4Options()
+{
+    PublicKey = mnemonic.Keys.PublicKey,
+};
+
+// create wallet instance
+WalletV4 wallet = new WalletV4(optionsV4, 2); 
+
+// creating options with onchain metadata 
+JettonMinterOptions options = new JettonMinterOptions()
+{
+    AdminAddress = wallet.Address,
+    JettonContent = new JettonOnChainContent()
+    {
+        Name = "My Best Token",
+        Symbol = "MBT",
+        Description = "This is the best description."
+        Decimals = 9, // optional
+        Image = "https://example.com/image.png" // optional
+    }
+};
+
+// creating jetton minter with options
+JettonMinter minter = new JettonMinter(options);
+
+// getting seqno using tonClient
+uint? seqno = await tonClient.Wallet.GetSeqno(wallet.Address);
+
+// creating jetton minter deploy message
+var msg = wallet.CreateTransferMessage(new[]
+{
+    new WalletTransfer
+    {
+        Message = new InternalMessage(new InternalMessageOptions
+        {
+            Info = new IntMsgInfo(new IntMsgInfoOptions
+            {
+                Dest = minter.Address,
+                Value = new Coins(0.05)
+            }),
+            Body = null,
+            StateInit = minter.StateInit
+        }),
+        Mode = 3 // message mode
+    }
+}, seqno ?? 0).Sign(m.Keys.PrivateKey);
+
+// send this message via TonClient
+await client.SendBoc(msg.Cell);
+
+// print jetton master contract
+Console.WriteLine(minter.Address);
+```
+
+#### Minting Jettons
+
+After that we created our Jetton Minter and deployed it, we can mint more jettons using `CreateMintRequest` method:
+
+```csharp
+// we will use same wallet and minter like in prev code block
+
+// create jetton mint options
+JettonMintOptions mintOptions = new JettonMintOptions()
+{
+    JettonAmount = new Coins(10000000), // jetton amount to mint
+    Amount = new Coins(0.05), // amount send to jetton wallet
+    Destination = wallet.Address // address which will get jettons
+};
+
+// create jetton mint request body
+Cell jettonMintBody = JettonMinter.CreateMintRequest(mintOptions);
+
+// getting seqno using tonClient
+uint? seqno = await tonClient.Wallet.GetSeqno(wallet.Address);
+
+// creating mint message
+var msg = wallet.CreateTransferMessage(new[]
+{
+    new WalletTransfer
+    {
+        Message = new InternalMessage(new InternalMessageOptions
+        {
+            Info = new IntMsgInfo(new IntMsgInfoOptions
+            {
+                Dest = minter.Address,
+                Value = new Coins(0.05)
+            }),
+            Body = jettonMintBody
+        }),
+        Mode = 3 // message mode
+    }
+}, seqno ?? 0).Sign(m.Keys.PrivateKey);
+
+// send this message via TonClient
+await client.SendBoc(msg.Cell);
+```
