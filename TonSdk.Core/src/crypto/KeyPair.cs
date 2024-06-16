42932cbb38682da82489bb2470bd611f84899c0e
witter-deland <87846830+witter-deland@users.noreply.github.com>
2024-06-16 10:28:08 +0800

    chore: make KeyPair usage simpler and more robust (#95)

--- TonSdk.Core/src/crypto/KeyPair.cs
+++ TonSdk.Core/src/crypto/KeyPair.cs
@@ -1,21 +1,33 @@
-﻿using Org.BouncyCastle.Crypto;
+﻿using System;
+using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Crypto.Signers;
 using TonSdk.Core.Boc;
 
-namespace TonSdk.Core.Crypto {
-    public class KeyPair {
+namespace TonSdk.Core.Crypto
+{
+    public class KeyPair
+    {
         private readonly byte[] _privateKey;
         private readonly byte[] _publicKey;
         public byte[] PrivateKey => _privateKey;
         public byte[] PublicKey => _publicKey;
 
-        public KeyPair(byte[] privateKey, byte[] publicKey) {
+        public KeyPair(byte[] privateKey, byte[] publicKey = null)
+        {
+            var privateKeyParams = new Ed25519PrivateKeyParameters(privateKey, 0);
+            var generatedPublicKey = privateKeyParams.GeneratePublicKey();
+            var encodedPublicKey = generatedPublicKey.GetEncoded();
+
+            if (encodedPublicKey != null && publicKey != encodedPublicKey)
+                throw new ArgumentException("Public key does not match the private key.");
+
             _privateKey = privateKey;
-            _publicKey = publicKey;
+            _publicKey = encodedPublicKey;
         }
 
-        private static byte[] SignDetached(byte[] hash, byte[] privateKey) {
+        private static byte[] SignDetached(byte[] hash, byte[] privateKey)
+        {
             Ed25519PrivateKeyParameters privateKeyParams = new Ed25519PrivateKeyParameters(privateKey, 0);
 
             ISigner signer = new Ed25519Signer();
@@ -33,7 +45,8 @@ namespace TonSdk.Core.Crypto {
         /// <param name="data">The Cell data to sign.</param>
         /// <param name="key">The key used for signing.</param>
         /// <returns>The signature of the hashed Cell data.</returns>
-        public static byte[] Sign(Cell data, byte[] key) {
+        public static byte[] Sign(Cell data, byte[] key)
+        {
             byte[] hash = data.Hash.ToBytes();
             byte[] signature = SignDetached(hash, key);
 
