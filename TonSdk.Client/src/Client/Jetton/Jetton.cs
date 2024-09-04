f98dd555d0741767395bf944090e6fcc21ae60a2
Ibrahim Abdulhussein <iah.develop@gmail.com>
2024-09-04 17:01:21 +0300

    Remove unused package

--- TonSdk.Client/src/Client/Jetton/Jetton.cs
+++ TonSdk.Client/src/Client/Jetton/Jetton.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Numerics;
-using System.Reflection.Metadata;
 using System.Threading.Tasks;
 using TonSdk.Client.Stack;
 using TonSdk.Core;
